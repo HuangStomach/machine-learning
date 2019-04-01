90267e5e81a0945c264a8707f3047871500d746d
HuangStomach <nxmbest@qq.com>
2019-04-01 21:44:04 +0800

    gradientDescentMulti

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,7 +16,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
+    theta = theta - transpose(alpha * (sum((X * theta - y) .* X) / m));
 
 
 

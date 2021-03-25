19fce9e29aa7372ff72c82c133befcbf2bd11a39
HuangStomach <nxmbest@qq.com>
2021-03-25 16:21:50 +0800

    random_forest

---
+++ python/2.3/cancer_random_forest.py
@@ -0,0 +1,27 @@
+import matplotlib
+matplotlib.use('MacOSX')
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=42)
+
+from sklearn.ensemble import RandomForestClassifier
+forest = RandomForestClassifier(n_estimators=100, random_state=0)
+forest.fit(X_train, y_train)
+
+def plot_feature_importances_cancer(model):
+    n_features = cancer.data.shape[1]
+    plt.barh(range(n_features), model.feature_importances_, align='center')
+    plt.yticks(np.arange(n_features), cancer.feature_names)
+    plt.xlabel("Feature importance")
+    plt.ylabel("Feature")
+
+plot_feature_importances_cancer(forest)
+
+plt.show()

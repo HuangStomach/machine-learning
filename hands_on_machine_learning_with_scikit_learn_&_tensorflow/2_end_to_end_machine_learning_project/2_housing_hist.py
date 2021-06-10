05256ace6d716df5dd10cb35cf3c9cdf928012a1
HuangStomach <nxmbest@qq.com>
2021-06-10 22:05:48 +0800

    2_end_to_end_machine_learning_project

--- hands_on_machine_learning_with_scikit_learn_&_tensorflow/2_end_to_end_machine_learning_project/2_housing_hist.py
+++ hands_on_machine_learning_with_scikit_learn_&_tensorflow/2_end_to_end_machine_learning_project/2_housing_hist.py
@@ -1,4 +1,5 @@
 import os
+import matplotlib.pyplot as plt
 import pandas as pd
 
 HOUSING_PATH = os.path.join("datasets", "housing")
@@ -8,6 +9,5 @@ def load_housing_data(housing_path=HOUSING_PATH):
     return pd.read_csv(csv_path)
 
 housing = load_housing_data()
-print(housing.head())
-print(housing.info())
-print(housing['ocean_proximity'].value_counts())
+housing.hist(bins=50, figsize=(12, 9))
+plt.show()

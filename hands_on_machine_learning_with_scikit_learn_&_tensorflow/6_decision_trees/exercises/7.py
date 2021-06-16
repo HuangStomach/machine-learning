9550d1d63802f1501eaf50a662b67ff4937e101b
HuangStomach <nxmbest@qq.com>
2021-06-16 22:06:00 +0800

    6 exercises 7

---
+++ hands_on_machine_learning_with_scikit_learn_&_tensorflow/6_decision_trees/exercises/7.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import make_moons
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
+
+X, y = make_moons(n_samples=10000, noise=0.4, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+params = {
+    'max_leaf_nodes': list(range(2, 100)), 
+    'min_samples_split': [2, 3, 4]
+}
+tree_clf = DecisionTreeClassifier(random_state=42)
+grid_search_cv = GridSearchCV(tree_clf, params, verbose=1, cv=3, n_jobs=-1)
+
+grid_search_cv.fit(X_train, y_train)
+grid_search_cv.best_estimator_
+
+print(grid_search_cv.best_estimator_.score(X_test, y_test))

59f8e6a314e1a2782098f1dc303c0a6062475e21
HuangStomach <nxmbest@qq.com>
2021-06-21 11:47:21 +0800

    9 exercises 12

---
+++ hands_on_machine_learning_with_scikit_learn_&_tensorflow/9_unsupervised_learning/exercises/12.py
@@ -0,0 +1,57 @@
+from sklearn.mixture import GaussianMixture
+from sklearn.datasets import fetch_olivetti_faces
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+
+olivetti = fetch_olivetti_faces()
+
+strat_split = StratifiedShuffleSplit(n_splits=1, test_size=40, random_state=42)
+train_valid_idx, test_idx = next(strat_split.split(olivetti.data, olivetti.target))
+X_train_valid = olivetti.data[train_valid_idx]
+y_train_valid = olivetti.target[train_valid_idx]
+X_test = olivetti.data[test_idx]
+y_test = olivetti.target[test_idx]
+
+strat_split = StratifiedShuffleSplit(n_splits=1, test_size=80, random_state=43)
+train_idx, valid_idx = next(strat_split.split(X_train_valid, y_train_valid))
+X_train = X_train_valid[train_idx]
+y_train = y_train_valid[train_idx]
+X_valid = X_train_valid[valid_idx]
+y_valid = y_train_valid[valid_idx]
+
+pca = PCA(0.99)
+X_train_pca = pca.fit_transform(X_train)
+X_valid_pca = pca.transform(X_valid)
+X_test_pca = pca.transform(X_test)
+
+gm = GaussianMixture(n_components=40, random_state=42)
+y_pred = gm.fit_predict(X_train_pca)
+
+n_gen_faces = 20
+gen_faces_reduced, y_gen_faces = gm.sample(n_samples=n_gen_faces)
+gen_faces = pca.inverse_transform(gen_faces_reduced)
+
+n_rotated = 4
+rotated = np.transpose(X_train[:n_rotated].reshape(-1, 64, 64), axes=[0, 2, 1])
+rotated = rotated.reshape(-1, 64*64)
+y_rotated = y_train[:n_rotated]
+
+n_flipped = 3
+flipped = X_train[:n_flipped].reshape(-1, 64, 64)[:, ::-1]
+flipped = flipped.reshape(-1, 64*64)
+y_flipped = y_train[:n_flipped]
+
+n_darkened = 3
+darkened = X_train[:n_darkened].copy()
+darkened[:, 1:-1] *= 0.3
+y_darkened = y_train[:n_darkened]
+
+X_bad_faces = np.r_[rotated, flipped, darkened]
+y_bad = np.concatenate([y_rotated, y_flipped, y_darkened])
+
+X_bad_faces_pca = pca.transform(X_bad_faces)
+gm.score_samples(X_bad_faces_pca)
+print(gm.score_samples(X_train_pca[:10]))

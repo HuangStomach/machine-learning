3152bb28054fa02b850493fb26310e5a49e46a3c
HuangStomach <nxmbest@qq.com>
2021-06-21 11:37:32 +0800

    9 exercises 10

---
+++ hands_on_machine_learning_with_scikit_learn_&_tensorflow/9_unsupervised_learning/exercises/10.py
@@ -0,0 +1,48 @@
+from sklearn.datasets import fetch_olivetti_faces
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+
+olivetti = fetch_olivetti_faces()
+
+strat_split = StratifiedShuffleSplit(n_splits=1, test_size=40, random_state=42)
+train_valid_idx, test_idx = next(strat_split.split(olivetti.data, olivetti.target))
+X_train_valid = olivetti.data[train_valid_idx]
+y_train_valid = olivetti.target[train_valid_idx]
+X_test = olivetti.data[test_idx]
+y_test = olivetti.target[test_idx]
+
+strat_split = StratifiedShuffleSplit(n_splits=1, test_size=80, random_state=43)
+train_idx, valid_idx = next(strat_split.split(X_train_valid, y_train_valid))
+X_train = X_train_valid[train_idx]
+y_train = y_train_valid[train_idx]
+X_valid = X_train_valid[valid_idx]
+y_valid = y_train_valid[valid_idx]
+
+pca = PCA(0.99)
+X_train_pca = pca.fit_transform(X_train)
+X_valid_pca = pca.transform(X_valid)
+X_test_pca = pca.transform(X_test)
+
+
+kmeans = KMeans(n_clusters=120, random_state=42).fit(X_train_pca)
+
+def plot_faces(faces, labels, n_cols=5):
+    faces = faces.reshape(-1, 64, 64)
+    n_rows = (len(faces) - 1) // n_cols + 1
+    plt.figure(figsize=(n_cols, n_rows * 1.1))
+    for index, (face, label) in enumerate(zip(faces, labels)):
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(face, cmap="gray")
+        plt.axis("off")
+        plt.title(label)
+    plt.show()
+
+for cluster_id in np.unique(kmeans.labels_):
+    print("Cluster", cluster_id)
+    in_cluster = kmeans.labels_==cluster_id
+    faces = X_train[in_cluster]
+    labels = y_train[in_cluster]
+    plot_faces(faces, labels)
